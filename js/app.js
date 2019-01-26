0e6a092ffc4c48a88d98b981ec37605c955395f7
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-01-26 13:43:45 -1000

    created new folders for css/js.

---
+++ js/app.js
@@ -0,0 +1,88 @@
+
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d'); 
+let ctxW = canvas.width;
+let ctxH = canvas.height;
+
+//get player id
+let playerX = document.getElementById('playerX');
+let playerY = document.getElementById('playerY');
+
+
+let witch = new Image();
+witch.src = 'assets/witch.png';
+
+let vampire = new Image();
+vampire.src = 'assets/vampire.png';
+
+let werewolf = new Image();
+werewolf.src = 'assets/werewolf.png';
+
+let witchX = 370;
+let witchY = 400;
+let vampireX = 500;
+let vampireY = 400;
+let werewolfX = 200;
+let werewolfY = 400;
+let playerXLoc = 270;
+let playerYLoc = 400;
+let playerSpeed = 2;
+
+
+
+let key = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+
+};
+
+let keys = {
+    38: false,
+    40: false,
+    37: false,
+    39: false,
+};
+
+window.addEventListener('keydown', e => {
+    if(keys[e.keyCode] !== 'undefined'){
+        keys[e.keyCode] = true;
+    };
+});
+
+window.addEventListener('keyup', e => {
+    if(keys[e.keyCode] !== 'undefined'){
+        keys[e.keyCode] = true;
+    };
+});
+
+const drawMovedImage = (image, x, y) => {
+    ctx.save();
+    ctx.translate(x, y);
+    ctx.drawImage(image, x, y);
+}
+
+// const step = () => {
+//     if(keys[key.Left]){witchX-=playerSpeed};
+//     if(keys[key.Right]){witchX+=playerSpeed};
+
+//     playerX.innerHTML = Math.floor(witchX);
+
+// };
+
+const draw = () => {
+    ctx.clearRect( 0, 0, ctxW, ctxH);
+    // drawMovedImage(witch, witchX, witchY);
+    ctx.drawImage(vampire,vampireX, vampireY);
+    ctx.drawImage(werewolf, werewolfX, werewolfY);
+};
+
+const frame = () => {
+    draw();
+    // step();
+    window.requestAnimationFrame(frame);
+};
+
+frame();
