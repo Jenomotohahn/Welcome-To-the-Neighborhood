84af3be7edfdacc805f57d982fd3bbf81eef3a86
Jimzy <JimzyLui@users.noreply.github.com>
2019-01-26 13:45:30 -1000

    snapshot

--- app.js
+++ app.js
@@ -1,51 +1,48 @@
+const qMgt = require("./qMgt.js");
 
-
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d'); 
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 let ctxW = canvas.width;
 let ctxH = canvas.height;
 
 //get player id
-let player = document.getElementById('Player');
-
+let player = document.getElementById("Player");
 
 let key = {
-    UP: 38,
-    DOWN: 40,
-    LEFT: 37,
-    RIGHT: 39,
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
 };
 
 let keys = {
-    38: false,
-    40: false,
-    37: false,
-    39: false,
+  38: false,
+  40: false,
+  37: false,
+  39: false
 };
 
-window.addEventListener('keydown', e => {
-    if(keys[e.keyCode] !== 'undefined'){
-        keys[e.keyCode] = true;
-    };
+window.addEventListener("keydown", e => {
+  if (keys[e.keyCode] !== "undefined") {
+    keys[e.keyCode] = true;
+  }
 });
 
-window.addEventListener('keyup', e => {
-    if(keys[e.keyCode] !== 'undefined'){
-        keys[e.keyCode] = true;
-    };
+window.addEventListener("keyup", e => {
+  if (keys[e.keyCode] !== "undefined") {
+    keys[e.keyCode] = true;
+  }
 });
 
-const step = () => {
-
-};
+const step = () => {};
 
 const draw = () => {
-    ctx.clearRect( 0, 0, ctxW, ctxH);
-    ctx.drawImage(image, 0,0);
-}
+  ctx.clearRect(0, 0, ctxW, ctxH);
+  ctx.drawImage(image, 0, 0);
+};
 
 const frame = () => {
-    window.requestAnimationFrame(frame);
+  window.requestAnimationFrame(frame);
 };
 
 frame();
---
+++ js/qMgt.js
@@ -0,0 +1,10 @@
+/*
+  Manage the question disbursal & response
+
+*/
+
+const arrGeo = require("./data/questions.geography.js");
+const arrGit = require("./data/questions.GIT.js");
+
+const panel = document.getElementById("questionPanel");
+// const loadQ = () => {};
