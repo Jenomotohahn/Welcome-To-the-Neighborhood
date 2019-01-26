2bcbf486a304448e3ec878b6febae204aaa57615
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2019-01-26 10:55:40 -1000

    added in player,witch,werewolf, building divs. created a canvas and the 4 movement buttons with event listeners

---
+++ app.js
@@ -0,0 +1,43 @@
+
+
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d'); 
+let ctxW = canvas.width;
+let ctxH = canvas.height;
+
+//get player id
+let player = document.getElementById('Player');
+
+
+let key = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39,
+};
+
+let keys = {
+    38: false,
+    40: false,
+    37: false,
+    39: false,
+};
+
+window.addEventListener('keydown', e => {
+    if(keys[e.keyCode] !== 'undefined'){
+        keys[e.keyCode] = true;
+    };
+});
+
+window.addEventListener('keyup', e => {
+    if(keys[e.keyCode] !== 'undefined'){
+        keys[e.keyCode] = true;
+    }
+});
+
+
+const frame = () => {
+    window.requestAnimationFrame(frame);
+};
+
+frame();
